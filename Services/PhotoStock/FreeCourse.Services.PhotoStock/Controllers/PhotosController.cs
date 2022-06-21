60787d4003b5e9553010e0d989abb1ae0118839f
Akif Kemal Aksu <akifkemalaksu@gmail.com>
2022-06-21 23:13:31 +0300

    PhotoStock Api final

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Matching;
+using System.Diagnostics.Contracts;
+using System.Net;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                    await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = $"photos/{photo.FileName}";
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, HttpStatusCode.OK));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty.", HttpStatusCode.NotFound));
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found.", HttpStatusCode.NotFound));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(HttpStatusCode.NoContent));
+        }
+    }
+}
