6ad6db04bf95314faa971d004a7f64f38b4b6762
Akif Kemal Aksu <akifkemalaksu@gmail.com>
2022-08-22 19:23:10 +0300

    web app developing

---
+++ Frontends/FreeCourse.Web/Program.cs
@@ -0,0 +1,54 @@
+using FreeCourse.Web.Handlers;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Services;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+
+builder.Services.AddHttpClient<IIdentityService, IdentityService>();
+builder.Services.AddHttpClient<IUserService, UserService>(options =>
+{
+    var serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+    options.BaseAddress = new Uri(serviceApiSettings.IdentityBaseUri);
+}).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
+builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/Auth/SignIn";
+    options.ExpireTimeSpan = TimeSpan.FromDays(60);
+    options.SlidingExpiration = true;
+    options.Cookie.Name = "udemywebcookie";
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Frontends/FreeCourse.Web/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using System.Drawing.Text;
+
+namespace FreeCourse.Web.Services
+{
+    public class UserService : IUserService
+    {
+        public readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserViewModel> GetUser() => await _httpClient.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
+    }
+}
